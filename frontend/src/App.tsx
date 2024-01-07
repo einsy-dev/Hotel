2bd0051659ef676f63fdb2660990d219fb193ef2
einsy-dev <eins.yeager@gmail.com>
2024-01-07 16:46:10 +0600

    stil calendar improvments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,7 @@ import Calendar from "./components/calendar";
 
 function App() {
   return (
-    <Container
-      className="bg-light border border-dark"
-      style={{ minHeight: "100vh" }}
-    >
+    <Container style={{ minHeight: "100vh" }}>
       <Calendar />
     </Container>
   );
--- frontend/src/components/calendar/index.tsx
+++ frontend/src/components/calendar/index.tsx
@@ -1,14 +1,10 @@
-import { useLayoutEffect, useState } from "react";
+import { useState } from "react";
 import moment from "moment";
 import { Button, Col, Container, Row } from "react-bootstrap";
 
 export default function Calendar() {
-  const [date, setDate] = useState(moment());
-  const [order, setOrder] = useState({ from: 0, to: 0 });
-
-  useLayoutEffect(() => {
-    console.log(order);
-  });
+  const [date, setDate] = useState(moment().startOf("month"));
+  const [order, setOrder] = useState({});
 
   return (
     <>
@@ -27,32 +23,67 @@ export default function Calendar() {
           {">>"}
         </Button>
       </Container>
-      <CalendarRender
-        start={date.clone().startOf("month").day()}
-        days={date.clone().daysInMonth()}
-      />
+      <CalendarRender date={date.clone()} setOrder={setOrder} order={order} />
     </>
   );
 }
 
-const CalendarRender = ({ start, days }: { start: number; days: number }) => {
-  const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const CalendarRender = ({
+  date,
+  order,
+  setOrder,
+}: {
+  date: moment.Moment;
+  order: any;
+  setOrder: any;
+}) => {
+  const start = date.startOf("month").day();
+  const days = date.daysInMonth();
+
+  const dayOfWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
   const emptyArr = new Array(start).fill("");
   const daysArr = Array.from({ length: days }, (_, i) => i + 1);
 
   const arr = splitArr(emptyArr.concat(daysArr), 7, dayOfWeek);
 
-  console.log(arr);
   return (
-    <Container className="w-50">
-      {arr.map((el) => (
-        <Row className="justify-content-md-start">
-          {el.map((i) => (
+    <Container style={{ width: "max-content" }}>
+      {arr.map((el, id) => (
+        <Row key={id}>
+          {el.map((i, index) => (
             <Col
-              className="m-0 border border-1 d-flex justify-content-center align-items-center"
-              style={{ height: "50px", width: "50px" }}
+              key={index}
+              {...(Number(i) && {
+                onClick: () => {
+                  setOrder((prev: any) => {
+                    let result = { ...prev };
+                    let dateSet = date.set("D", i);
+                    if (prev.from && prev.to && prev.from.isSame(prev.to)) {
+                      if (dateSet.isAfter(prev.from)) {
+                        result.to = dateSet;
+                      } else if (dateSet.isBefore(prev.from)) {
+                        result.from = dateSet;
+                      }
+                    } else {
+                      result = { from: dateSet, to: dateSet };
+                    }
+                    return result;
+                  });
+                },
+              })}
             >
-              {i}
+              <Button
+                className="d-flex justify-content-center align-items-center rounded-4 btn"
+                variant="secondary"
+                style={{ width: "50px", height: "50px" }}
+                {...(Number(i) && {
+                  active:
+                    date.set("D", i).isSameOrAfter(order.from) &&
+                    date.set("D", i).isSameOrBefore(order.to),
+                })}
+              >
+                {i}
+              </Button>
             </Col>
           ))}
         </Row>
